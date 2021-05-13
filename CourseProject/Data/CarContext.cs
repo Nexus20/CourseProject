af08518a0a3b23e4a367d9abb5d7b5dd02da1e9f
Voidwalker20 <chubarov.zheka@gmail.com>
2021-05-13 23:27:25 +0300

    Create DB context

---
+++ CourseProject/Data/CarContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseProject.Data {
+    public class CarContext : DbContext {
+
+        public CarContext(DbContextOptions<CarContext> options) : base(options) {}
+
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<CarModel> CarModels { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<FuelType> FuelTypes { get; set; }
+        public DbSet<BodyType> BodyTypes { get; set; }
+        public DbSet<TransmissionType> TransmissionTypes { get; set; }
+
+    }
+}
