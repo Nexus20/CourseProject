6356ceadbb9fd40acc801e86a5f9dd9e48aa0c34
Voidwalker20 <chubarov.zheka@gmail.com>
2021-05-13 23:14:49 +0300

    Add models
    Add models: Car, CarModel, Brand, FuelType, TransmissionType, BodyType

---
+++ CourseProject/Models/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseProject.Models {
+    public class Car {
+
+        public int ID { get; set; }
+
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        public enum CarState {
+            New,
+            SecondHand
+        }
+
+        public CarState State { get; set; }
+
+        public int ModelID { get; set; }
+        public CarModel Model { get; set; }
+
+        public double? EngineVolume { get; set; }
+        public double Mileage { get; set; }
+
+        public int FuelTypeID { get; set; }
+        public FuelType FuelType { get; set; }
+
+        public int BodyTypeID { get; set; }
+        public BodyType BodyType { get; set; }
+
+        public int TransmissionTypeID { get; set; }
+        public TransmissionType TransmissionType { get; set; }
+
+    }
+}
---
+++ CourseProject/Models/CarModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseProject.Models {
+    public class CarModel {
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int BrandID { get; set; }
+        public Brand Brand { get; set; }
+
+        public int ParentModelID { get; set; }
+        public Model ParentModel { get; set; }
+
+    }
+}
