972640510ad0c4e103b08df369a1524c47cf1e3f
Voidwalker20 <chubarov.zheka@gmail.com>
2021-05-14 02:54:52 +0300

    Update Program.cs
    Initialize DB in program.cs

--- CourseProject/Data/CarContext.cs
+++ CourseProject/Data/CarContext.cs
@@ -23,6 +23,7 @@ namespace CourseProject.Data {
             modelBuilder.Entity<Car>().ToTable("Car");
             modelBuilder.Entity<CarModel>().ToTable("CarModel");
             modelBuilder.Entity<CarModel>().Property(model => model.ParentModelID).HasDefaultValue(0);
+
             modelBuilder.Entity<Brand>().ToTable("Brand");
             modelBuilder.Entity<FuelType>().ToTable("FuelType");
             modelBuilder.Entity<BodyType>().ToTable("BodyType");
