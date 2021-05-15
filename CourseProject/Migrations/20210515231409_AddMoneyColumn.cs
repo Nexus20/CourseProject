c52f1bfff798da5fb781a564f32944518feb3eb2
Voidwalker20 <chubarov.zheka@gmail.com>
2021-05-16 02:15:20 +0300

    Change type of price column in Car model to money type

---
+++ CourseProject/Migrations/20210515231409_AddMoneyColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseProject.Migrations
+{
+    public partial class AddMoneyColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Car",
+                type: "money",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Car",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "money");
+        }
+    }
+}
--- CourseProject/Models/Car.cs
+++ CourseProject/Models/Car.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace CourseProject.Models {
         public int Id { get; set; }
 
         public int Year { get; set; }
+
+        [Column(TypeName = "money")]
         public decimal Price { get; set; }
 
         public enum CarState {
