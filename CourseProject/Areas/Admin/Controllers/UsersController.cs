ed7f4c78767b35508c914a6ffcc57db3effd599a
Voidwalker20 <chubarov.zheka@gmail.com>
2021-05-18 00:04:02 +0300

    Add basic user management
    Add user creating/editing/deleting

---
+++ CourseProject/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseProject.Models;
+using CourseProject.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace CourseProject.Areas.Admin.Controllers {
+
+    [Area("Admin")]
+    public class UsersController : Controller {
+        private readonly UserManager<User> _userManager;
+
+        public UsersController(UserManager<User> userManager) {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index() {
+            return View(_userManager.Users.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserViewModel model) {
+            if (ModelState.IsValid) {
+                var user = new User { Email = model.Email, UserName = model.UserName };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded) {
+                    return RedirectToAction("Index");
+                }
+                else {
+                    foreach (var error in result.Errors) {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id) {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) {
+                return NotFound();
+            }
+            EditUserViewModel model = new EditUserViewModel { Id = user.Id, Email = user.Email, UserName = user.UserName};
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditUserViewModel model) {
+            if (ModelState.IsValid) {
+                var user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null) {
+                    user.Email = model.Email;
+                    user.UserName = model.UserName;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded) {
+                        return RedirectToAction("Index");
+                    }
+                    else {
+                        foreach (var error in result.Errors) {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id) {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null) {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
