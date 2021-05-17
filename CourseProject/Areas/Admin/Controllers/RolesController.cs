e05d72b83ffd2822895c237fb3ec76cf046b40b0
Voidwalker20 <chubarov.zheka@gmail.com>
2021-05-18 01:36:04 +0300

    Add role management

---
+++ CourseProject/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CourseProject.Models;
+using CourseProject.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace CourseProject.Areas.Admin.Controllers {
+
+    [Area("Admin")]
+    public class RolesController : Controller {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager) {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index() {
+            return View(_roleManager.Roles.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string name) {
+            if (!string.IsNullOrEmpty(name)) {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded) {
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in result.Errors) {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id) {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if (role != null) {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UserList() {
+            return View(_userManager.Users.ToList());
+        }
+
+        public async Task<IActionResult> Edit(string userId) {
+            // Get the user
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null) {
+                // Get roles of the user 
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                ChangeRoleViewModel model = new ChangeRoleViewModel {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string userId, List<string> roles) {
+            // Get the user
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user != null) {
+                // Get roles of the user
+                var userRoles = await _userManager.GetRolesAsync(user);
+                // Get all roles
+                var allRoles = _roleManager.Roles.ToList();
+                // Get added roles
+                var addedRoles = roles.Except(userRoles);
+                // Get deleted roles
+                var removedRoles = userRoles.Except(roles);
+
+                await _userManager.AddToRolesAsync(user, addedRoles);
+
+                await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+                return RedirectToAction("UserList");
+            }
+
+            return NotFound();
+        }
+    }
+}
+
--- CourseProject/Models/ViewModels/ChangeRoleViewModel.cs
+++ CourseProject/Models/ViewModels/ChangeRoleViewModel.cs
@@ -9,9 +9,9 @@ namespace CourseProject.Models.ViewModels {
     public class ChangeRoleViewModel {
 
         public string UserId { get; set; }
-        public string UserEmail { get; set; }
+        public string UserName { get; set; }
         public List<IdentityRole> AllRoles { get; set; }
-        public List<string> UserRoles { get; set; }
+        public IList<string> UserRoles { get; set; }
 
         public ChangeRoleViewModel() {
             AllRoles = new List<IdentityRole>();
