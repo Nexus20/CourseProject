a89fc80931130b976ea9c4b8796df0450419976e
Voidwalker20 <chubarov.zheka@gmail.com>
2021-05-18 22:24:21 +0300

    Add featured car model

---
+++ CourseProject/Models/FeaturedCar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseProject.Models {
+    public class FeaturedCar {
+
+        public int Id { get; set; }
+
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
