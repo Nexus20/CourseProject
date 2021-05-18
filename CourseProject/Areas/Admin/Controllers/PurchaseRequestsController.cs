a0c34ae7c819274a978adff80d644bd5ff9d08d8
Voidwalker20 <chubarov.zheka@gmail.com>
2021-05-18 12:13:07 +0300

    Add state changing for request

--- CourseProject/Areas/Admin/Controllers/PurchaseRequestsController.cs
+++ CourseProject/Areas/Admin/Controllers/PurchaseRequestsController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CourseProject.Data;
+using CourseProject.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -33,7 +34,60 @@ namespace CourseProject.Areas.Admin.Controllers {
                 .ThenInclude(cm => cm.Parent)
                 .AsNoTracking();
 
+            ViewBag.PurchaseRequestStates = Enum.GetValues(typeof(PurchaseRequest.RequestState)).Cast<int>().ToDictionary(key => key, key => Enum.GetName(typeof(PurchaseRequest.RequestState), key));
+            
             return View(await requests.ToListAsync());
         }
+
+        public async Task<IActionResult> Details(int? id) {
+
+            if (id == null) {
+                return NotFound();
+            }
+
+            var request = await _context.PurchaseRequests
+                .Include(pr => pr.Car)
+                    .ThenInclude(c => c.Model)
+                        .ThenInclude(cm => cm.Brand)
+                .Include(pr => pr.Car)
+                    .ThenInclude(c => c.Model)
+                        .ThenInclude(cm => cm.Parent)
+                .Include(pr => pr.Car)
+                    .ThenInclude(c => c.BodyType)
+                .Include(pr => pr.Car)
+                    .ThenInclude(c => c.FuelType)
+                .Include(pr => pr.Car)
+                    .ThenInclude(c => c.TransmissionType)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(pr => pr.Id == id);
+
+            if (request == null) {
+                return NotFound();
+            }
+
+            ViewBag.PurchaseRequestStates = Enum.GetValues(typeof(PurchaseRequest.RequestState)).Cast<int>().ToDictionary(key => key, key => Enum.GetName(typeof(PurchaseRequest.RequestState), key));
+
+            return View(request);
+        }
+
+        public IActionResult ChangeRequestState(int? requestId, int? newState) {
+
+            if (requestId == null || newState == null) {
+                return NotFound();
+            }
+
+            var request = _context.PurchaseRequests.FirstOrDefault(pr => pr.Id == requestId);
+
+            if (request == null) {
+                return NotFound();
+            }
+
+            request.State = (PurchaseRequest.RequestState)newState.Value;
+            _context.Update(request);
+            _context.SaveChanges();
+
+            return Content(Enum.GetName(typeof(PurchaseRequest.RequestState), (PurchaseRequest.RequestState)newState.Value));
+        }
+
     }
 }
